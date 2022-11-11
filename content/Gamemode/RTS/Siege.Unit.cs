89a50e0d9e5e33fde5ca97a5f93a352bb4a91589
nuclearmayhem1 <53118215+nuclearmayhem1@users.noreply.github.com>
2022-11-11 03:27:57 +0100

    First commit
    Created a database for units and a UI for loading selecting them

---
+++ content/Gamemode/RTS/Siege.Unit.cs
@@ -0,0 +1,56 @@
+﻿using Keg.Extensions;
+using TC2.Base.Components;
+
+namespace TC2.Siege
+{
+	public partial interface IUnit: IAsset2<IUnit, IUnit.Data>
+	{
+		static void IAsset2<IUnit, IUnit.Data>.OnUpdate(IUnit.Definition definition, ref IUnit.Data data_new)
+		{
+
+		}
+
+		static void IAsset2<IUnit, IUnit.Data>.OnInit(out string prefix, out string[] extensions, out int capacity_world, out int capacity_region, out int capacity_local)
+		{
+			prefix = "unit.";
+			extensions = new string[]
+			{
+				".hjson"
+			};
+
+			capacity_world = 64;
+			capacity_region = 64;
+			capacity_local = 64;
+		}
+
+		[Serializable]
+		public partial struct Data
+		{
+			public string name;
+			public string desc;
+			public Sprite icon;
+
+			public int price;
+			public int reward;
+
+			public Prefab.Handle creature;
+
+			public Prefab.Handle[] items;
+			public Prefab.Handle[] equipment;
+			public (string,int)[] material;
+
+			public Data(string name, string desc, Sprite icon, int price, int reward, Prefab.Handle creature, Prefab.Handle[] items, Prefab.Handle[] equipment, (string, int)[] material)
+			{
+				this.name = name;
+				this.desc = desc;
+				this.icon = icon;
+				this.price = price;
+				this.reward = reward;
+				this.creature = creature;
+				this.items = items;
+				this.equipment = equipment;
+				this.material = material;
+			}
+		}
+	}
+}
--- content/Gamemode/Siege.GUI.cs
+++ content/Gamemode/Siege.GUI.cs
@@ -91,25 +91,141 @@ namespace TC2.Siege
 		}
 #endif
 
+
+		[IComponent.Data(Net.SendType.Unreliable)]
+		public partial struct WaveDesigner: IComponent
+		{
+			public FixedArray32<IUnit.Handle> units = new FixedArray32<IUnit.Handle>();
+
+			public WaveDesigner()
+			{
+
+			}
+		}
+
+		public partial struct AddUnitRPC: Net.IRPC<Siege.WaveDesigner>
+		{
+			public IUnit.Handle unit;
+
+#if SERVER
+			public void Invoke(ref NetConnection connection, Entity entity, ref Siege.WaveDesigner wave)
+			{
+				for (int i = 0; i < wave.units.Length; i++)
+				{
+					if (wave.units[i].IsNull())
+					{
+						wave.units[i] = this.unit;
+					}
+				}
+				wave.Sync(entity);
+			}
+#endif
+		}
+
+		[IComponent.Data(Net.SendType.Unreliable)]
+		public partial struct ControllerData: IComponent
+		{
+			public int money;
+
+			public ControllerData(int money)
+			{
+				this.money = money;
+			}
+		}
+
+		public partial struct ChangeMoney: Net.IRPC<Siege.ControllerData>
+		{
+			public int change;
+
+#if SERVER
+			public void Invoke(ref NetConnection connection, Entity entity, ref Siege.ControllerData data)
+			{
+				data.money += this.change;
+				data.Sync(entity);
+			}
+#endif
+		}
+
 #if CLIENT
 		public partial struct SiegeAttackerGUI: IGUICommand
 		{
 			public Siege.Gamemode g_siege;
 			public Siege.Gamemode.State g_siege_state;
+			public Entity entity;
+			public static List<uint> unit_indices = new List<uint>(64);
+			public static float scale = 2;
 
 			public void Draw()
 			{
-				var window_pos = (GUI.CanvasSize * new Vector2(0.50f, 0.00f)) + new Vector2(100, 48);
-				using (var window = GUI.Window.Standalone("Siege2", position: window_pos, size: new Vector2(100, 100), pivot: new Vector2(0.50f, 0.00f)))
-				{
-					this.StoreCurrentWindowTypeID();
-					if (window.show)
-					{
-						ref var region = ref Client.GetRegion();
-						ref var world = ref Client.GetWorld();
-						ref var game_info = ref Client.GetGameInfo();
 
-						GUI.Title($"{this.g_siege_state.faction_defenders.id}");
+				using (var widget = Sidebar.Widget.New("attacker_build", "Recruit", "ui_icon_build", new Vector2(300, 600), lockable: false))
+				{
+					if (widget.show)
+					{
+						GUI.Title("Purchase units");
+						using (GUI.Scrollbox.New("attacker_unitshop", GUI.GetAvailableSize()))
+						{
+							using (var grid = GUI.Grid.New(size: GUI.GetRemainingSpace()))
+							{
+								unit_indices.Clear();
+								var recipes = IUnit.Database.GetAssets();
+								foreach (var d_recipe in recipes)
+								{
+									ref var recipe = ref d_recipe.GetData();
+									if (recipe.IsNotNull())
+									{
+										unit_indices.Add(d_recipe.id);
+									}
+								}
+
+								foreach (var pair in unit_indices)
+								{
+									//GUI.Text($"{pair.rank}");
+
+									ref var recipe = ref IUnit.Database.GetData(pair);
+									if (recipe.IsNotNull())
+									{
+										var frame_size = recipe.icon.GetFrameSize(scale);
+										frame_size += new Vector2(8, 8);
+										frame_size = frame_size.ScaleToNearestMultiple(new Vector2(48, 48));
+
+										grid.Push(frame_size);
+										using (var button = GUI.CustomButton.New(recipe.name, frame_size, sound: GUI.sound_select, sound_volume: 0.10f))
+										{
+											GUI.Draw9Slice(GUI.tex_slot_simple, new Vector4(4), button.bb);
+											GUI.DrawSpriteCentered(recipe.icon, button.bb, scale: scale);
+
+											if (button.pressed)
+											{
+												var rpc = new Siege.AddUnitRPC
+												{
+													unit = new IUnit.Handle(pair),
+												};
+												rpc.Send(this.entity);
+
+												var rpc1 = new Siege.ChangeMoney
+												{
+													change = -recipe.price,
+												};
+												rpc1.Send(this.entity);
+											}
+										}
+										if (GUI.IsItemHovered())
+										{
+											using (GUI.Tooltip.New())
+											{
+												using (GUI.Wrap.Push(325))
+												{
+													GUI.Title(recipe.name);
+													GUI.Text(recipe.desc, color: GUI.font_color_default);
+													GUI.DrawMoney(recipe.price, new Vector2(8, 8));
+												}
+											}
+										}
+									}
+								}
+							}
+						}
 					}
 				}
 			}
@@ -123,7 +239,8 @@ namespace TC2.Siege
 				var gui = new SiegeAttackerGUI()
 				{
 					g_siege = g_siege,
-					g_siege_state = g_siege_state
+					g_siege_state = g_siege_state,
+					entity = entity,
 				};
 				gui.Submit();
 			}
